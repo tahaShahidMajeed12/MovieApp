bca46718251921695585aa42f888e6132494da5b
Taha Shahid <taha.shahaid@devigital.com>
2021-09-20 17:29:59 +0500

    container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Footer from "./components/Footer";
+import Footer from "./shared/Footer";
 import SearchBox from "./components/SearchBoxComponent";
-import NavBar from "./components/AppBar";
-import CardList from "./components/CardList";
+import Card from "./shared/Card";
+import NavBar from "./components/Header";
 export default function App() {
   return (
     <>
       <NavBar />
       <SearchBox />
-      <CardList />
+      <Card />
       <Footer />
     </>
   );
